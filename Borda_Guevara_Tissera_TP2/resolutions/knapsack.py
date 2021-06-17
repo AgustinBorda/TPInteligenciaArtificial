928a991dadf904c12707064c2ff84568cab764bc
Agustin Borda <agustinborda1998@gmail.com>
2021-06-17 17:33:18 -0300

    Functional knapsack problem
    Signed-off-by: Agustin Borda <agustinborda1998@gmail.com>

--- Borda_Guevara_Tissera_TP2/resolutions/knapsack.py
+++ Borda_Guevara_Tissera_TP2/resolutions/knapsack.py
@@ -2,31 +2,40 @@ from search import Problem
 
 
 class KnapsackState:
-    def __init__(self, capacity, weight, value):
+    def __init__(self, capacity, weight, value, obj_in):
         self.capacity = capacity
         self.weight = weight
         self.value = value
+        self.objects_in = obj_in
 
-    def add_item(self, item):
-        return KnapsackState(self.capacity, self.weight + item[0], self.value + item[1])
+    def add_item(self, item, object_place):
+        obj_in = self.objects_in.copy()
+        obj_in[object_place] = True
+        return KnapsackState(self.capacity, self.weight + item[0], self.value + item[1], obj_in)
 
-# TODO: eficiencia de las listas, hacer que solo se pueda meter un elemento por vez (listas en el state)
 
 class KnapsackProblem(Problem):
 
     def __init__(self, capacity, objects):
-        self.initial = KnapsackState(capacity, 0, 0)
+        self.initial = KnapsackState(capacity, 0, 0, [False for i in range(len(objects))])
         self.knapsack_objects = objects
 
     def actions(self, state):
         available_items = []
-        for t in self.knapsack_objects:
-            if state.weight + t[0] <= state.capacity:  # t[0] == The weight of item t
-                available_items.append(t)
+        for i in range(len(self.knapsack_objects)):
+            if (self.knapsack_objects[i][0] + state.weight <= state.capacity) and not state.objects_in[i]:
+                available_items.append((self.knapsack_objects[i], i))
         return available_items
 
     def result(self, state, action):
-        return state.add_item(action)
+        return state.add_item(action[0], action[1])
 
     def value(self, state):
         return state.value
+
+    def objects(self, state):
+        obj = []
+        for i in range(len(state.objects_in)):
+            if state.objects_in[i]:
+                obj.append(self.knapsack_objects[i])
+        return obj
--- Borda_Guevara_Tissera_TP2/tests/test_knapsack.py
+++ Borda_Guevara_Tissera_TP2/tests/test_knapsack.py
@@ -1,5 +1,5 @@
 from Borda_Guevara_Tissera_TP2.resolutions.knapsack import KnapsackProblem, KnapsackState
-from search import hill_climbing
+from search import hill_climbing, InstrumentedProblem
 
 
 def test_knapsack_problem_initial():
@@ -10,31 +10,41 @@ def test_knapsack_problem_initial():
 
 
 def test_knapsack_add():
-    knapsack = KnapsackState(20, 0, 0)
-    added_knapsack = knapsack.add_item((10, 15))
+    knapsack = KnapsackState(20, 0, 0, [False])
+    added_knapsack = knapsack.add_item((10, 15), 0)
     assert added_knapsack.capacity == knapsack.capacity
     assert added_knapsack.value == 15
     assert added_knapsack.weight == 10
     assert knapsack.value == 0
     assert knapsack.weight == 0
+    assert added_knapsack.objects_in[0]
+    assert not knapsack.objects_in[0]
 
 
 def test_knapsack_actions():
     problem = KnapsackProblem(20, [(25, 30), (5, 9), (20, 200)])
     actions = problem.actions(problem.initial)
-    assert actions.__contains__((5, 9))
-    assert actions.__contains__((20, 200))
-    assert not actions.__contains__((25, 30))
+    assert actions.__contains__(((5, 9), 1))
+    assert actions.__contains__(((20, 200), 2))
+    assert not actions.__contains__(((25, 30), 0))
 
 
 def test_knapsack_value():
     problem = KnapsackProblem(20, [(25, 30), (5, 9), (20, 200)])
-    state = KnapsackState(20, 20, 20)
+    state = KnapsackState(20, 20, 20, [False])
     assert problem.value(state) == state.value
 
 
 def test_knapsack_hill_climbing_1():
-    problem = KnapsackProblem(100, [(15, 20), (50, 1), (1, 1000), (2, 500), (1, 1001)])
-    result = hill_climbing(problem)
-    assert result.value == 100100
-    assert result.weight == result.capacity == 100
+    problem = KnapsackProblem(100, [(115, 20), (50, 1), (1, 1000), (2, 500), (1, 1001)])
+    ins_problem = InstrumentedProblem(problem)
+    result = hill_climbing(ins_problem)
+    assert result.value == 2502
+    assert result.weight == 54
+    assert result.capacity == 100
+    obj = ins_problem.objects(result)
+    assert obj.__contains__((50, 1))
+    assert obj.__contains__((1, 1000))
+    assert obj.__contains__((2, 500))
+    assert obj.__contains__((1, 1001))
+    assert not obj.__contains__((115, 20))
